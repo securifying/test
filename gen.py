ee4b95cdf79aac50e965df09569fef5d48fd06ae
Dhaval Gujar <saynojee@gmail.com>
2022-05-20 10:16:58 +0530

    ESP Game

--- gen.py
+++
@@ -1,3 +0,0 @@
-for j in range(0,10):
-    for i in range(0,841):
-        print("a{}".format(j+1))
--- participation_script.sh
+++
@@ -1,4 +0,0 @@
-pdftk A=participation_all.pdf cat A1-318 output Certificates/p1.pdf
-pdftk A=participation_all.pdf cat A319-637 output Certificates/p2.pdf
-pdftk A=participation_all.pdf cat A638-955 output Certificates/p3.pdf
-pdftk A=participation_all.pdf cat A956-1274 output Certificates/p4.pdf
